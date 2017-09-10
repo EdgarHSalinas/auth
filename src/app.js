687c7d695f5f9e944f79f9baa626b58e354384eb
Edgar H Salinas <edgar@my.codergv.org>
2017-09-10 13:50:57 -0500

    CardSection Input Email

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import firebase from 'firebase';
 import Config from 'react-native-config';
 import { Header } from './components/common';
