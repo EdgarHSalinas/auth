4d66ca18341f3037139c43b3660fdd1a1db6d7f9
Edgar H Salinas <edgar@my.codergv.org>
2017-09-10 11:53:29 -0500

    added eslint and generic components

---
+++ src/components/common/index.js
@@ -0,0 +1,6 @@
+//import { Button, Card, CardSection, Header } from './components/index'; 
+
+export * from './Button';
+export * from './Card';
+export * from './CardSection';
+export * from './Header';
