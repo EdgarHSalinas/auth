e5ee04d1c4b2c7fa7d6eabe697742c7af125342a
Edgar H Salinas <edgar@my.codergv.org>
2017-09-11 17:37:28 -0500

    spinner and onLoginSuccess

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import firebase from 'firebase';
 import Config from 'react-native-config';
+import firebase from 'firebase';
 import { Header } from './components/common';
 import LoginForm from './components/LoginForm';
 
@@ -16,9 +16,11 @@ class App extends Component {
                 storageBucket: Config.STORAGE_BUCKET,
                 messagingSenderId: Config.MESSAGING_SENDER_ID
         });
+        console.log(Config.API_KEY);
     }
     render() {
         return (
+            
             <View>
                 <Header headerText="Authentication" />
                 <LoginForm />
